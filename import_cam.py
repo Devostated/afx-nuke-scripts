3612d7deaa2db131de9b6e3dcc46f5621b77c8e0
Devo <devomvm@gmail.com>
2024-12-01 11:47:59 +0100

    CamIO import speed
    ## Changelog
    - Major import speed improvement, thanks to @herronelou on the nuke coven discord!
    
    Co-Authored-By: Erwan Leroy <1182452+herronelou@users.noreply.github.com>

--- import_cam.py
+++ import_cam.py
@@ -1,6 +1,5 @@
-import nuke_internal as nuke
+import nuke
 import math
-
 
 def smoothEulerRotationKeys(rotKeys):
     smoothedKeys = []
@@ -35,145 +34,116 @@ def AlienSwarm_FovScaling(width, height, fov):
 
 def createCam():
     camera = nuke.createNode("Camera3")
-    camera['rotate'].setAnimated()
-    camera['translate'].setAnimated()
-    camera['focal'].setAnimated()
-    # get selected nodes
-    selected_nodes = nuke.selectedNodes()
-
-    # deselect all nodes so the camera doesn't link
-    for n in selected_nodes:
+    # Deselect all nodes so the camera doesn't link
+    for n in nuke.selectedNodes():
         n["selected"].setValue(False)
-
     return camera
 
 
 def readCam(filePath):
-    file = open(filePath)
-    words = ReadLineWords(file)
+    with open(filePath) as file:
+        version = 0
+        scaleFov = ''
+        frame = 0
 
-    version = 0
-    scaleFov = ''
-    frame = 0
+        last_time = None
 
-    if not (2 <= len(words) and 'advancedfx' == words[0] and 'Cam' == words[1]):
-        nuke.alert('Not an valid advancedfx Cam file.')
-        return False
-
-    while True:
         words = ReadLineWords(file)
-        if (1 <= len(words)):
-            if 'DATA' == words[0]:
+        if not (len(words) == 2 and 'advancedfx' == words[0] and 'Cam' == words[1]):
+            nuke.alert('Not a valid advancedfx Cam file.')
+            return False
+
+        # Read metadata
+        while True:
+            words = ReadLineWords(file)
+            if len(words) >= 1 and words[0] == 'DATA':
                 break
-            if 'version' == words[0] and 2 <= len(words):
-                version = int(words[1])
-            if 'scaleFov' == words[0] and 2 <= len(words):
-                scaleFov = words[1]
+            if len(words) >= 2:
+                if words[0] == 'version':
+                    version = int(words[1])
+                elif words[0] == 'scaleFov':
+                    scaleFov = words[1]
 
-    if (version < 1 or version > 2):
-        nuke.alert("Invalid version, only 1 - 2 are supported.")
-        return False
+        if version < 1 or version > 2:
+            nuke.alert("Invalid version, only 1 - 2 are supported.")
+            return False
 
-    if not (scaleFov in ['', 'none', 'alienSwarm']):
-        nuke.alert("Unsupported scaleFov value.")
-        return False
+        if scaleFov not in ['', 'none', 'alienSwarm']:
+            nuke.alert("Unsupported scaleFov value.")
+            return False
 
-    root = nuke.Root()
-    fps = root.fps()
-    format = root.format()
-    width = format.width()
-    height = format.height()
+        root = nuke.Root()
+        fps = root.fps()
+        format = root.format()
+        width = format.width()
+        height = format.height()
 
-    camera = createCam()
+        camera = createCam()
 
-    last_time = None
+        last_time = None
 
-    # Get animation curves for each axis
-    xPosCurve = camera['translate'].animation(0)
-    yPosCurve = camera['translate'].animation(1)
-    zPosCurve = camera['translate'].animation(2)
+        # Create temporary knobs
+        temp_rotate = nuke.XYZ_Knob("temp_rotate", "Temporary Rotate")
+        temp_translate = nuke.XYZ_Knob("temp_translate", "Temporary Translate")
+        temp_focal = nuke.Double_Knob("temp_focal", "Temporary Focal")
 
-    xRotCurve = camera['rotate'].animation(0)
-    yRotCurve = camera['rotate'].animation(1)
-    zRotCurve = camera['rotate'].animation(2)
+        temp_rotate.setAnimated()
+        temp_translate.setAnimated()
+        temp_focal.setAnimated()
 
-    fovCurve = camera['focal'].animation(0)
-    camera['vaperture'].value() == camera['haperture'].value() / width * height
+        camera['vaperture'].value() == camera['haperture'].value() / width * height
 
-    # Prepare lists to collect keys for batch addition
-    xKeys, yKeys, zKeys = [], [], []
-    xRotKeys, yRotKeys, zRotKeys = [], [], []
-    fovKeys = []
+        # Populate temporary knobs
+        while True:
+            words = ReadLineWords(file)
+            if len(words) < 8:
+                break
 
-    while True:
-        words = ReadLineWords(file)
-        if not (8 <= len(words)):
-            break
+            time = float(words[0])
 
-        time = float(words[0])
+            if last_time is None:
+                last_time = time
 
-        if last_time is None:
-            last_time = time
+            time = time - last_time
+            frame = time * fps
 
-        time = time - last_time
-        frame = time * fps
+            xpos, ypos, zpos = map(float, words[1:4])
+            xrot, yrot, zrot = map(float, words[4:7])
+            fov = float(words[7])
 
-        xPos = float(words[1])
-        yPos = float(words[2])
-        zPos = float(words[3])
+            # none and alienSwarm was confused in version 1, version 2 always outputs real fov and doesn't have scaleFov.
+            if 'none' == scaleFov:
+                fov = AlienSwarm_FovScaling(width, height, fov)
 
-        xRot = float(words[4])
-        yRot = float(words[5])
-        zRot = float(words[6])
+            lens = camera['haperture'].value() / (2.0 * math.tan(math.radians(fov) / 2.0))
 
-        fov = float(words[7])
+            rotKeys_y = [(frame, -yrot)]
+            rotKeys_z = [(frame, zrot)]
+            rotKeys_x = [(frame, -xrot)]
 
-        # none and alienSwarm was confused in version 1, version 2 always outputs real fov and doesn't have scaleFov.
-        if 'none' == scaleFov:
-            fov = AlienSwarm_FovScaling(width, height, fov)
+            xrotSmoothed = smoothEulerRotationKeys(rotKeys_y)
+            yrotSmoothed = smoothEulerRotationKeys(rotKeys_z)
+            zrotSmoothed = smoothEulerRotationKeys(rotKeys_x)
 
-        lens = camera['haperture'].value(
-        ) / (2.0 * math.tan(math.radians(fov) / 2.0))
+            temp_rotate.setValueAt(xrotSmoothed[0][1], frame, 0)
+            temp_rotate.setValueAt(yrotSmoothed[0][1], frame, 1)
+            temp_rotate.setValueAt(zrotSmoothed[0][1], frame, 2)
 
-        # Collect keyframe data and convert units to meters
-        xKeys.append((frame, -yPos * 0.0254))
-        yKeys.append((frame, zPos * 0.0254))
-        zKeys.append((frame, -xPos * 0.0254))
+            temp_translate.setValueAt(-ypos * 0.0254, frame, 0)
+            temp_translate.setValueAt(zpos * 0.0254, frame, 1)
+            temp_translate.setValueAt(-xpos * 0.0254, frame, 2)
 
-        xRotKeys.append((frame, -yRot))
-        yRotKeys.append((frame, zRot))
-        zRotKeys.append((frame, -xRot))
+            temp_focal.setValueAt(lens, frame, 0)
 
-        fovKeys.append((frame, lens))
-
-    xRotKeys = smoothEulerRotationKeys(xRotKeys)
-    yRotKeys = smoothEulerRotationKeys(yRotKeys)
-    zRotKeys = smoothEulerRotationKeys(zRotKeys)
-
-    for frame, value in xKeys:
-        xPosCurve.setKey(frame, value)
-
-    for frame, value in yKeys:
-        yPosCurve.setKey(frame, value)
-
-    for frame, value in zKeys:
-        zPosCurve.setKey(frame, value)
-
-    for frame, value in xRotKeys:
-        xRotCurve.setKey(frame, value)
-
-    for frame, value in yRotKeys:
-        yRotCurve.setKey(frame, value)
-
-    for frame, value in zRotKeys:
-        zRotCurve.setKey(frame, value)
-
-    for frame, value in fovKeys:
-        fovCurve.setKey(frame, value)
+        # Transfer animations to real knobs
+        camera["rotate"].fromScript(temp_rotate.toScript())
+        camera["translate"].fromScript(temp_translate.toScript())
+        camera["focal"].fromScript(temp_focal.toScript())
 
     root['last_frame'].setValue(frame)
     nuke.tprint("Imported Camio camera from: " + filePath)
 
+    return True
 
 def importCamio():
     p = nuke.Panel('Import CamIO (.cam)')
@@ -181,10 +151,8 @@ def importCamio():
     p.addButton('Cancel')
     p.addButton('OK')
 
-    ret = p.show()
-
-    filePath = p.value('Select CamIO File:')
-    if ret:
+    if p.show():
+        filePath = p.value('Select CamIO File:')
         if not filePath.endswith('.cam'):
             nuke.alert("Please select a file with a '.cam' extension")
             return False
